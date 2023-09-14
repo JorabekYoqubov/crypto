d805a8a7daf10b442d1f8439d2137308e7736dfc
Yakubov Jo'rabek <107387452+JorabekYoqubov@users.noreply.github.com>
2023-09-14 09:56:08 +0500

    Add files via upload

---
+++ assests/js/main.js
@@ -0,0 +1,36 @@
+gsap.from('main', {
+    duraction: 0.5,
+    delay: 0.5,
+    y: -20,
+    opacity: 0,
+})
+gsap.from('.text_section', {
+    duraction: 0.5,
+    delay: 0.5,
+    scale: 0.7,
+    opacity: 0,
+})
+gsap.from('.image_section', {
+    duraction: 0.5,
+    delay: 0.5,
+    y: 20,
+    opacity: 0,
+})
+gsap.from('.gsap_a_1', {
+    x: 30,
+    delay: 1.4,
+    opacity: 0,
+    duraction: 1.4,
+})
+gsap.from('.text_button', {
+    x: -30,
+    duraction: 0.8,
+    delay: 0.8,
+    opacity: 0,
+})
+gsap.from('.gsap_h1_1', {
+    scale: 1.6,
+    delay:1,
+    duraction: 1,
+    opacity: 0
+})
